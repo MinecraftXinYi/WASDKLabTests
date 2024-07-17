1fca2a953fa8a430c62358062791cfbb4d1fa6c9
yi xin <3077501050@qq.com>
2024-07-17 13:36:26 +0800

    添加项目文件。

---
+++ WASLoaderTestApp/WASLoaderTestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using WinRT;
+using Microsoft.UI.Xaml;
+using WASLoaderTestApp;
+
+namespace WASLoaderTest
+{
+    public static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            if (!RuntimeHelper.IsMSIX)
+            {
+                if (!WARInitializer.InitializeWAR(out int hresult))
+                {
+                    System.Environment.Exit(hresult);
+                }
+            }
+            ComWrappersSupport.InitializeComWrappers();
+            Application.Start((p) => new App());
+            return;
+        }
+    }
+}
---
+++ WASLoaderTestApp/WASLoaderTestApp/WARInitializer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Windows.ApplicationModel.DynamicDependency;
+using System;
+using System.IO;
+using System.Text.Json;
+
+internal static class WARInitializer
+{
+    public static bool InitializeWAR(out int HResult)
+    {
+        try
+        {
+            WASVersionInfoFromConfig wasVersionInfo = new();
+            bool result = Bootstrap.TryInitialize
+                (wasVersionInfo.MajorMinorVersion, wasVersionInfo.VersionTag, wasVersionInfo.MinVersion,
+                Bootstrap.InitializeOptions.OnNoMatch_ShowUI, out HResult);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            HResult = ex.HResult;
+            return false;
+        }
+    }
+
+    public class WASVersionInfoFromConfig
+    {
+        private readonly JsonElement config;
+        private readonly JsonDocumentOptions jsonDocumentOptions = new()
+        {
+            AllowTrailingCommas = true,
+            CommentHandling = JsonCommentHandling.Skip,
+        };
+
+        public WASVersionInfoFromConfig()
+        {
+            string configString = File.ReadAllText("WASDKVersionInfo.json");
+            JsonDocument configDoc = JsonDocument.Parse(configString, jsonDocumentOptions);
+            config = configDoc.RootElement.GetProperty("WASDKVersionInfo");
+        }
+
+        public uint MajorMinorVersion
+        {
+            get
+            {
+                string rawdata = config.GetProperty("MajorMinorVersion").GetString();
+                return Convert.ToUInt32(rawdata, 16);
+            }
+        }
+
+        public PackageVersion MinVersion
+        {
+            get
+            {
+                string rawdata = config.GetProperty("MinVersion").GetString();
+                return new PackageVersion(Convert.ToUInt64(rawdata, 16));
+            }
+        }
+
+        public string VersionTag
+        {
+            get
+            {
+                return config.GetProperty("VersionTag").GetString();
+            }
+        }
+    }
+}
